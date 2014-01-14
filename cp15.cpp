f8cc06de6cb9e3eeaff91525f7bad10b1ca4704c
Gabriel Krisman Bertazi <gabriel@krisman.be>
2014-01-14 10:33:26 -0200

    Rewrite cp15 to use hash table on register bank. Access became faster, but no secure access levels is implemented.

--- cp15.cpp
+++ cp15.cpp
@@ -33,6 +33,8 @@ extern bool DEBUG_CP15;
 
 cp15::cp15 (sc_module_name name_): sc_module (name_)
 {
+
+  memset (registers, 0, 0xFFFF);
   reset ();
 }
 
@@ -41,412 +43,107 @@ cp15::reset ()
 {
   dprintf ("CP15 reseted to default values\n");
 
-  //SCC
-  //Control
-  SCC_RB[CTR].value = 0x00C50078;	//Depends on external
-  SCC_RB[CTR].permissions[arm_impl::PL0] = (NO_ACCESS);
-  SCC_RB[CTR].permissions[arm_impl::PL1] = (CL_READ | CL_WRITE);
-  //Auxiliar Control
-  SCC_RB[AUX_CTR].value = 0x00000002;
-  SCC_RB[AUX_CTR].permissions[arm_impl::PL0] = (NO_ACCESS);
-  SCC_RB[AUX_CTR].permissions[arm_impl::PL1] = (CL_READ | CL_WRITE);
+  registers[MAIN_ID].value = 0x412fc080;
+  registers[CACHE_TYPE].value = 0x80048004;
+  registers[TCM_TYPE].value = 0x000000;
+  registers[TLB_TYPE].value = 0x00202001;
+  registers[MULTIPROCESSOR_ID].value = 0x00000000;
 
-  //Secure Configuration
-  SCC_RB[SEC_CONF].value = 0x00000000;
-  SCC_RB[SEC_CONF].permissions[arm_impl::PL0] = (NO_ACCESS);
-  SCC_RB[SEC_CONF].permissions[arm_impl::PL1] = (CL_READ | CL_WRITE);
+  registers[PROCESSOR_FEATURE_0].value = 0x00001031;
+  registers[PROCESSOR_FEATURE_1].value = 0x00000011;
+  registers[DEBUG_FEATURE_0].value = 0x00000400;
+  registers[AUXILIARY_FEATURE_0].value = 0x00000000;
+  registers[MEMORY_MODEL_FEATURE_0].value = 0x31100003;
+  registers[MEMORY_MODEL_FEATURE_1].value = 0x20000000;
+  registers[MEMORY_MODEL_FEATURE_2].value = 0x01202000;
+  registers[MEMORY_MODEL_FEATURE_3].value = 0x00000011;
 
-  //Secure Debug Enable
-  SCC_RB[SEC_DBG_ENABLE].value = 0x00000000;
-  SCC_RB[SEC_DBG_ENABLE].permissions[arm_impl::PL0] = (NO_ACCESS);
-  SCC_RB[SEC_DBG_ENABLE].permissions[arm_impl::PL1] = (CL_READ | CL_WRITE);
+  registers[INSTRUCTION_SET_ATTRIBUTE_0].value = 0x00101111;
+  registers[INSTRUCTION_SET_ATTRIBUTE_1].value = 0x12112111;
+  registers[INSTRUCTION_SET_ATTRIBUTE_2].value = 0x21232031;
+  registers[INSTRUCTION_SET_ATTRIBUTE_3].value = 0x11112131;
+  registers[INSTRUCTION_SET_ATTRIBUTE_4].value = 0x00011142;
+  registers[INSTRUCTION_SET_ATTRIBUTE_5].value = 0x00000000;
+  registers[INSTRUCTION_SET_ATTRIBUTE_6].value = 0x00000000;
+  registers[INSTRUCTION_SET_ATTRIBUTE_7].value = 0x00000000;
 
-  //NonSecure Access Control
-  SCC_RB[NONSEC_ACC_CTR].value = 0x00000000;
-  SCC_RB[NONSEC_ACC_CTR].permissions[arm_impl::PL0] = (NO_ACCESS);
-  SCC_RB[NONSEC_ACC_CTR].permissions[arm_impl::PL1] = (CL_READ | CL_WRITE);
+  registers[CACHE_LEVEL_ID].value = 0x0a000023;
+  registers[SILICON_ID].value = 0x00000000;
 
-  //Coprocessor access control
-  SCC_RB[COPROC_ACC_CTR].value = 0x00000000;
-  SCC_RB[COPROC_ACC_CTR].permissions[arm_impl::PL0] = (NO_ACCESS);
-  SCC_RB[COPROC_ACC_CTR].permissions[arm_impl::PL1] = (CL_READ | CL_WRITE);
+  registers[CONTROL].value = 0x00c50078;
+  registers[AUXILIARY_CONTROL].value = 0x00000002;
+  registers[COPROCESSOR_ACCESS_CONTROL].value = 0x00000000;
+  registers[SECURE_CONFIGURATION].value = 0x00000000;
+  registers[SECURE_DEBUG_ENABLED].value = 0x00000000;
+  registers[NONSECURE_ACCESS_CONTROL].value = 0x00000000;
 
-  //Secure & nonsecure vector base
-  SCC_RB[SEC_NONSEC_VEC_BASE].value = 0x00000000;
-  SCC_RB[SEC_NONSEC_VEC_BASE].permissions[arm_impl::PL0] = (NO_ACCESS);
-  SCC_RB[SEC_NONSEC_VEC_BASE].permissions[arm_impl::PL1] =
-    (CL_READ | CL_WRITE);
+  registers[PHYSICAL_ADDRESS].value = 0x00000000 ;
 
-  //Monitor vector base address
-  SCC_RB[MON_VEC_BASE_ADD].value = 0x00000000;
-  SCC_RB[MON_VEC_BASE_ADD].permissions[arm_impl::PL0] = (NO_ACCESS);
-  SCC_RB[MON_VEC_BASE_ADD].permissions[arm_impl::PL1] = (CL_READ | CL_WRITE);
+  registers[PERFORMANCE_MONITOR_CONTROL].value = 0x41002000 ;
+  registers[COUNT_ENABLE_SET].value = 0x00000000;
+  registers[COUNT_ENABLE_CLEAR].value = 0x00000000;
+  registers[OVERFLOW_FLAG_STATUS].value = 0x00000000;
+  registers[SOFTWARE_INCREMENT].value = 0x00000000;
+  registers[CYCLE_COUNT].value = 0x00000000;
 
-  //Main ID
-  SCC_RB[MAIN_ID].value = 0x412FC081;
-  SCC_RB[MAIN_ID].permissions[arm_impl::PL0] = (NO_ACCESS);
-  SCC_RB[MAIN_ID].permissions[arm_impl::PL1] = (CL_READ);
+  registers[PERFORMANCE_MONITOR_COUNT].value = 0x00000000;
+  registers[USER_ENABLE].value = 0x00000000;
+  registers[INTERRUPT_ENABLE_SET].value = 0x00000000;
+  registers[INTERRUPT_ENABLE_CLEAR].value = 0x00000000;
 
-  //Silicon ID
-  SCC_RB[SILICON_ID].value = 0x00000000;	//Depends on external
-  SCC_RB[SILICON_ID].permissions[arm_impl::PL0] = (NO_ACCESS);
-  SCC_RB[SILICON_ID].permissions[arm_impl::PL1] = (CL_READ);
+  registers[L2_CACHE_LOCKDOWN].value = 0x00000000;
+  registers[L2_CACHE_AUXILIARY_CONTROL].value = 0x00000042;
 
-  //Memory model feature
-  SCC_RB[MEM_MODEL_FEAT_0].value = 0x31100003;
-  SCC_RB[MEM_MODEL_FEAT_0].permissions[arm_impl::PL0] = (NO_ACCESS);
-  SCC_RB[MEM_MODEL_FEAT_0].permissions[arm_impl::PL1] = (CL_READ);
+  registers[DATA_TLB_LOCKDOWN_REGISTER].value = 0x00000000;
+  registers[INSTRUCTION_TLB_LOCKDOWN_REGISTER].value = 0x00000000;
+  registers[PRIMARY_REGION_REMAP_REGISTER].value = 0x00098aa4;
+  registers[NORMAL_MEMORY_REMAP_REGISTER].value = 0x44304830;
 
-  //Instruction Set Attribute 0-7
-  SCC_RB[IS_ATTR_0].value = 0x00101111;
-  SCC_RB[IS_ATTR_0].permissions[arm_impl::PL0] = (NO_ACCESS);
-  SCC_RB[IS_ATTR_0].permissions[arm_impl::PL1] = (CL_READ);
+  registers[SECURE_OR_NONSECURE_VECTOR_BASE_ADDRESS].value = 0x00000000;
+  registers[MONITOR_VECTOR_BASE_ADDRESS].value = 0x00000000;
+  registers[INTERRUPT_STATUS].value = 0x00000000;
 
-  SCC_RB[IS_ATTR_1].value = 0x13112111;
-  SCC_RB[IS_ATTR_1].permissions[arm_impl::PL0] = (NO_ACCESS);
-  SCC_RB[IS_ATTR_1].permissions[arm_impl::PL1] = (CL_READ);
-
-  SCC_RB[IS_ATTR_2].value = 0x21232031;
-  SCC_RB[IS_ATTR_2].permissions[arm_impl::PL0] = (NO_ACCESS);
-  SCC_RB[IS_ATTR_2].permissions[arm_impl::PL1] = (CL_READ);
-
-  SCC_RB[IS_ATTR_3].value = 0x11112131;
-  SCC_RB[IS_ATTR_3].permissions[arm_impl::PL0] = (NO_ACCESS);
-  SCC_RB[IS_ATTR_3].permissions[arm_impl::PL1] = (CL_READ);
-
-  SCC_RB[IS_ATTR_4].value = 0x00011142;
-  SCC_RB[IS_ATTR_4].permissions[arm_impl::PL0] = (NO_ACCESS);
-  SCC_RB[IS_ATTR_4].permissions[arm_impl::PL1] = (CL_READ);
-
-  SCC_RB[IS_ATTR_5].value = 0x00000000;
-  SCC_RB[IS_ATTR_5].permissions[arm_impl::PL0] = (NO_ACCESS);
-  SCC_RB[IS_ATTR_5].permissions[arm_impl::PL1] = (CL_READ);
-
-  SCC_RB[IS_ATTR_6].value = 0x00000000;
-  SCC_RB[IS_ATTR_6].permissions[arm_impl::PL0] = (NO_ACCESS);
-  SCC_RB[IS_ATTR_6].permissions[arm_impl::PL1] = (CL_READ);
-
-  SCC_RB[IS_ATTR_7].value = 0x00000000;
-  SCC_RB[IS_ATTR_7].permissions[arm_impl::PL0] = (NO_ACCESS);
-  SCC_RB[IS_ATTR_7].permissions[arm_impl::PL1] = (CL_READ);
-
-  //MMU
-  //TLB type
-  MMU_RB[TLB_TYPE].value = 0x00202001;
-  MMU_RB[TLB_TYPE].permissions[arm_impl::PL0] = (NO_ACCESS);
-  MMU_RB[TLB_TYPE].permissions[arm_impl::PL1] = (CL_READ);
-
-  //Translation table 0 base
-  MMU_RB[TTB_0].value = 0x00000000;	//Unpredictable
-  MMU_RB[TTB_0].permissions[arm_impl::PL0] = (NO_ACCESS);
-  MMU_RB[TTB_0].permissions[arm_impl::PL1] = (CL_READ | CL_WRITE);
-
-  //translation table 1 base
-  MMU_RB[TTB_1].value = 0x00000000;	//Unpredictable
-  MMU_RB[TTB_1].permissions[arm_impl::PL0] = (NO_ACCESS);
-  MMU_RB[TTB_1].permissions[arm_impl::PL1] = (CL_READ | CL_WRITE);
-
-  //Translation table control
-  MMU_RB[TTB_CTR].value = 0x00000000;	//Unpredictable
-  MMU_RB[TTB_CTR].permissions[arm_impl::PL0] = (NO_ACCESS);
-  MMU_RB[TTB_CTR].permissions[arm_impl::PL1] = (CL_READ | CL_WRITE);
-
-  //Domain access control
-  MMU_RB[DOMAIN_ACC_CTR].value = 0x00000000;	//Unpredictable
-  MMU_RB[DOMAIN_ACC_CTR].permissions[arm_impl::PL0] = (NO_ACCESS);
-  MMU_RB[DOMAIN_ACC_CTR].permissions[arm_impl::PL1] = (CL_READ | CL_WRITE);
-
-  //Data Fault Status Register
-  MMU_RB[DFSR].value = 0x00000000;	//Unpredictable
-  MMU_RB[DFSR].permissions[arm_impl::PL0] = (NO_ACCESS);
-  MMU_RB[DFSR].permissions[arm_impl::PL1] = (CL_READ | CL_WRITE);
-
-  //Data Fault Status Auxiliar Register
-  MMU_RB[DFSR_AUX].value = 0x00000000;	//Unpredictable
-  MMU_RB[DFSR_AUX].permissions[arm_impl::PL0] = (NO_ACCESS);
-  MMU_RB[DFSR_AUX].permissions[arm_impl::PL1] = (CL_READ | CL_WRITE);
-
-  //Instruction Fault Status Register
-  MMU_RB[IFSR].value = 0x00000000;	//Unpredictable
-  MMU_RB[IFSR].permissions[arm_impl::PL0] = (NO_ACCESS);
-  MMU_RB[IFSR].permissions[arm_impl::PL1] = (CL_READ | CL_WRITE);
-
-  //Instruction Fault Status Auxiliar Register
-  MMU_RB[IFSR_AUX].value = 0x00000000;	//Unpredictable
-  MMU_RB[IFSR_AUX].permissions[arm_impl::PL0] = (NO_ACCESS);
-  MMU_RB[IFSR_AUX].permissions[arm_impl::PL1] = (CL_READ | CL_WRITE);
-
-  //Instruction Fault Address Register
-  MMU_RB[IFAR].value = 0x00000000;	//Unpredictable
-  MMU_RB[IFAR].permissions[arm_impl::PL0] = (NO_ACCESS);
-  MMU_RB[IFAR].permissions[arm_impl::PL1] = (CL_READ | CL_WRITE);
-
-  //Data fault address register
-  MMU_RB[DFAR].value = 0x00000000;	//Unpredictable
-  MMU_RB[DFAR].permissions[arm_impl::PL0] = (NO_ACCESS);
-  MMU_RB[DFAR].permissions[arm_impl::PL1] = (CL_READ | CL_WRITE);
-
-  //Primary region remap
-  MMU_RB[PRIMARY_REGION_REMAP].value = 0x00098AA4;
-  MMU_RB[PRIMARY_REGION_REMAP].permissions[arm_impl::PL0] = (NO_ACCESS);
-  MMU_RB[PRIMARY_REGION_REMAP].permissions[arm_impl::PL1] =
-    (CL_READ | CL_WRITE);
-
-  //Normal Region Remap
-  MMU_RB[NORMAL_REGION_REMAP].value = 0x44E048E0;
-  MMU_RB[NORMAL_REGION_REMAP].permissions[arm_impl::PL0] = (NO_ACCESS);
-  MMU_RB[NORMAL_REGION_REMAP].permissions[arm_impl::PL1] =
-    (CL_READ | CL_WRITE);
-
-  //Context ID
-  MMU_RB[CONTEXT_ID].value = 0x00000000;	//Unpredictable
-  MMU_RB[CONTEXT_ID].permissions[arm_impl::PL0] = (NO_ACCESS);
-  MMU_RB[CONTEXT_ID].permissions[arm_impl::PL1] = (CL_READ | CL_WRITE);
-
-  //FCSE PID
-  MMU_RB[FCSE_PID].value = 0x00000000;
-  MMU_RB[FCSE_PID].permissions[arm_impl::PL0] = (NO_ACCESS);
-  MMU_RB[FCSE_PID].permissions[arm_impl::PL1] = (CL_READ | CL_WRITE);
-
-  //Thread and process ID registers
-  MMU_RB[TPID_RW].value = 0x00000000;	//Unpredictable
-  MMU_RB[TPID_RW].permissions[arm_impl::PL0] = (CL_READ | CL_WRITE);
-  MMU_RB[TPID_RW].permissions[arm_impl::PL1] = (CL_READ | CL_WRITE);
-
-  MMU_RB[TPID_RO].value = 0x00000000;	//Unpredictable
-  MMU_RB[TPID_RO].permissions[arm_impl::PL0] = (CL_READ);
-  MMU_RB[TPID_RO].permissions[arm_impl::PL1] = (CL_READ);
-
-  MMU_RB[TPID_PO].value = 0x00000000;	//Unpredictable
-  MMU_RB[TPID_PO].permissions[arm_impl::PL0] = (NO_ACCESS);
-  MMU_RB[TPID_PO].permissions[arm_impl::PL1] = (CL_READ | CL_WRITE);
-  //
+  registers[FCSE_PID].value = 0x00000000;
 }
 
-cp15::cp15_reg *
-  cp15::getRegister (const unsigned opc1, const unsigned opc2,
-		     const unsigned crn, const unsigned crm)
+cp15::cp15_register *
+cp15::getRegister (const unsigned opc1, const unsigned opc2,
+                   const unsigned crn, const unsigned crm)
 {
-  if (crn == 0)
-    {
-      if (opc1 == 0)
-	{
-	  if (crm == 0)
-	    {
-	      if (opc2 == 0)
-		return &(SCC_RB[MAIN_ID]);
-	      if (opc2 == 3)
-		return &(MMU_RB[TLB_TYPE]);
-	      if (opc2 == 4)
-		return &(SCC_RB[MAIN_ID]);
-	      if (opc2 == 6)
-		return &(SCC_RB[MAIN_ID]);
-	      if (opc2 == 7)
-		return &(SCC_RB[MAIN_ID]);
-	    }
-	  if (crm == 1)
-	    {
-	      if (opc2 == 4)
-		return &(SCC_RB[MEM_MODEL_FEAT_0]);
-	    }
-	  if (crm == 2)
-	    {
-	      if (opc2 == 0)
-		return &(SCC_RB[IS_ATTR_0]);
-	      if (opc2 == 1)
-		return &(SCC_RB[IS_ATTR_1]);
-	      if (opc2 == 2)
-		return &(SCC_RB[IS_ATTR_2]);
-	      if (opc2 == 3)
-		return &(SCC_RB[IS_ATTR_3]);
-	      if (opc2 == 4)
-		return &(SCC_RB[IS_ATTR_4]);
-	      if (opc2 == 5)
-		return &(SCC_RB[IS_ATTR_5]);
-	      if (opc2 == 6)
-		return &(SCC_RB[IS_ATTR_6]);
-	      if (opc2 == 7)
-		return &(SCC_RB[IS_ATTR_7]);
-	    }
-	}
-      if (opc1 == 1)
-	{
-	  if (crm == 0)
-	    {
-	      if (opc2 == 7)
-		return &(SCC_RB[SILICON_ID]);
-	    }
-	}
-    }
-  if (crn == 1)
-    {
-      if (opc1 == 0)
-	{
-	  if (crm == 0)
-	    {
-	      if (opc2 == 0)
-		return &(SCC_RB[CTR]);
-	      if (opc2 == 1)
-		return &(SCC_RB[AUX_CTR]);
-	      if (opc2 == 2)
-		return &(SCC_RB[COPROC_ACC_CTR]);
-	    }
-	  if (crm == 1)
-	    {
-	      if (opc2 == 0)
-		return &(SCC_RB[SEC_CONF]);
-	      if (opc2 == 1)
-		return &(SCC_RB[SEC_DBG_ENABLE]);
-	      if (opc2 == 2)
-		return &(SCC_RB[NONSEC_ACC_CTR]);
-	    }
-	}
-    }
-  if (crn == 2)
-    {
-      if (opc1 == 0)
-	{
-	  if (crm == 0)
-	    {
-	      if (opc2 == 0)
-		return &(MMU_RB[TTB_0]);
-	      if (opc2 == 1)
-		return &(MMU_RB[TTB_1]);
-	      if (opc2 == 2)
-		return &(MMU_RB[TTB_CTR]);
-	    }
-	}
-    }
-  if (crn == 3)
-    {
-      if (opc1 == 0)
-	{
-	  if (crm == 0)
-	    {
-	      if (opc2 == 0)
-		return &(MMU_RB[DOMAIN_ACC_CTR]);
-	    }
-	}
-    }
-  if (crn == 5)
-    {
-      if (opc1 == 0)
-	{
-	  if (crm == 0)
-	    {
-	      if (opc2 == 0)
-		return &(MMU_RB[DFSR]);
-	      if (opc2 == 1)
-		return &(MMU_RB[IFSR]);
-	    }
-	  if (crm == 1)
-	    {
-	      if (opc2 == 0)
-		return &(MMU_RB[DFSR_AUX]);
-	      if (opc2 == 1)
-		return &(MMU_RB[IFSR_AUX]);
-	    }
-	}
-    }
-  if (crn == 6)
-    {
-      if (opc1 == 0)
-	{
-	  if (crm == 0)
-	    {
-	      if (opc2 == 0)
-		return &(MMU_RB[DFAR]);
-	      if (opc2 == 2)
-		return &(MMU_RB[IFAR]);
-	    }
-	}
-    }
-  if (crn == 10)
-    {
-      if (opc1 == 0)
-	{
-	  if (crm == 2)
-	    {
-	      if (opc2 == 0)
-		return &(MMU_RB[PRIMARY_REGION_REMAP]);
-	      if (opc2 == 1)
-		return &(MMU_RB[NORMAL_REGION_REMAP]);
-	    }
-	}
-    }
-  if (crn == 12)
-    {
-      if (opc1 == 0)
-	{
-	  if (crm == 0)
-	    {
-	      if (opc2 == 0)
-		return &(SCC_RB[SEC_NONSEC_VEC_BASE]);
-	      if (opc2 == 1)
-		return &(SCC_RB[MON_VEC_BASE_ADD]);
-	    }
-	}
-    }
-  if (crn == 13)
-    {
-      if (opc1 == 0)
-	{
-	  if (crm == 0)
-	    {
-	      if (opc2 == 0)
-		return &(MMU_RB[FCSE_PID]);
-	      if (opc2 == 1)
-		return &(MMU_RB[CONTEXT_ID]);
-	      if (opc2 == 2)
-		return &(MMU_RB[TPID_RW]);
-	      if (opc2 == 3)
-		return &(MMU_RB[TPID_RO]);
-	      if (opc2 == 4)
-		return &(MMU_RB[TPID_PO]);
-	    }
-	}
-    }
-  return NULL;
+  unsigned h = (((crn & 0xf) << 12)
+                | ((opc1 & 0xf) << 8)
+                | ((crm & 0xf) << 4)
+                | (opc2 & 0xf));
+  return &(registers[h]);
 }
 
 void
 cp15::MCR (arm_arch_ref * core,
 	   const arm_impl::PrivilegeLevel pl, const unsigned opc1,
 	   const unsigned opc2, const unsigned crn,
-	   const unsigned crm, const unsigned rt_value)
+	   const unsigned crm, unsigned rt_value)
 {
   dprintf ("%s: MCR: "
 	   "opc1=0x%X, opc2=0x%X, crn=0X%X, crm=0x%X, RegVal=0x%X\n",
 	   this-> name(), opc1, opc2, crn, crm, rt_value);
 
-  if (crn == 8)
-    {
-      //Trap TLB instructions
-      TLB_operations (opc1, opc2, crn, crm, rt_value);
-      return;
-    }
-
-  cp15_reg *dest = getRegister (opc1, opc2, crn, crm);
-  if (dest == NULL)
-    {
-      fprintf (stderr,
-	       "%s: access to non-implemented cp15 register."
-	       "Operation: opc1=0x%X, opc2=0x%X, crn=0X%X, crm=0x%X\n",
-	       this->name(), opc1, opc2, crn, crm);
-      return;
-    }
+  struct cp15_register *reg = getRegister (opc1, opc2, crn, crm);
 
   // Check write permission
-  if (!(dest->permissions[pl] & CL_WRITE))
-    {
-      //caller has no permission to write to this register
-      //Generate Unidentified interruption
-      dprintf ("%s: Write not allowed. Low privilege level!", this->name());
-      service_interrupt (*core, arm_impl::EXCEPTION_UNDEFINED_INSTR);
+  // if (!(reg->permissions[pl] & CL_WRITE))
+  //   {
+  //     //caller has no permission to write to this register
+  //     //Generate Unidentified interruption
+  //     dprintf ("%s: Write not allowed. Low privilege level!", this->name());
+  //     service_interrupt (*core, arm_impl::EXCEPTION_UNDEFINED_INSTR);
+  //   }
 
-    }
+  if (reg->write_callback)
+    reg->write_callback (reg, &rt_value);
 
-  (*dest).value = rt_value;
+  // Commit write to register.
+  reg->value = rt_value;
+
   return;
 }
 
@@ -457,36 +154,20 @@ cp15::MRC (arm_arch_ref * core, const arm_impl::PrivilegeLevel pl,
 {
   dprintf ("%s: MRC: opc1=0x%X, opc2=0x%X, crn=0X%X, crm=0x%X\n",
 	   this->name(), opc1, opc2, crn, crm);
-  cp15_reg *dest = getRegister (opc1, opc2, crn, crm);
 
-  if (dest == NULL)
-    {
-      fprintf (stderr, "%s: Access to non-implemented cp15 register."
-	       "operation: opc1=0x%X, opc2=0x%X, crn=0X%X, crm=0x%X\n",
-	       this->name(), opc1, opc2, crn, crm);
-
-      return 0;
-    }
+  struct cp15_register *reg = getRegister (opc1, opc2, crn, crm);
 
   // Check read permission
-  if (!(dest->permissions[pl] & CL_READ))
-    {
-      //caller has no permission to read this register
-      //Generate Unidentified interruption
-      dprintf ("%s: Read not allowed. Low privilege level!\n", this->name());
-      service_interrupt (*core, arm_impl::EXCEPTION_UNDEFINED_INSTR);
-    }
-  return ((*dest).value);
-}
+  // if (!(reg->permissions[pl] & CL_READ))
+  //   {
+  //     //caller has no permission to read this register
+  //     //Generate Unidentified interruption
+  //     dprintf ("%s: Read not allowed. Low privilege level!\n", this->name());
+  //     service_interrupt (*core, arm_impl::EXCEPTION_UNDEFINED_INSTR);
+  //   }
 
+  if (reg->read_callback)
+    reg->read_callback (reg);
 
-void
-cp15::TLB_operations (const unsigned opc1, const unsigned opc2,
-		      const unsigned crn, const unsigned crm,
-		      const unsigned rt_value)
-{
-  dprintf ("%s: TLB Operations not implemented in this model (yet):"
-	   "opc1=0x%X, opc2=0x%X, crn=0x%X, crm=0x%X\n",
-	   this->name(), opc1, opc2, crn, crm);
-  return;
+  return reg->value;
 }
--- cp15.h
+++ cp15.h
@@ -33,48 +33,162 @@
 
 class cp15: public coprocessor, public sc_module
 {
-  typedef enum
-  { CTR = 0, AUX_CTR, SEC_CONF, SEC_DBG_ENABLE, NONSEC_ACC_CTR,
-    COPROC_ACC_CTR, SEC_NONSEC_VEC_BASE, MON_VEC_BASE_ADD,
-    MAIN_ID, SILICON_ID, MEM_MODEL_FEAT_0, IS_ATTR_0, IS_ATTR_1,
-    IS_ATTR_2, IS_ATTR_3, IS_ATTR_4, IS_ATTR_5, IS_ATTR_6,
-    IS_ATTR_7
-  } SCC_regNAME;
+  static const int MAIN_ID                      = 0x0000;
+  static const int CACHE_TYPE                   = 0x0001;
+  static const int TCM_TYPE                     = 0x0002;
+  static const int TLB_TYPE                     = 0x0003;
+  static const int MULTIPROCESSOR_ID            = 0x0005;
 
-  typedef enum
-  { TLB_TYPE, TTB_0, TTB_1, TTB_CTR, DOMAIN_ACC_CTR, DFSR,
-    DFSR_AUX, IFSR, IFSR_AUX, IFAR, DFAR,
-    CONTEXT_ID, FCSE_PID, TPID_RW, TPID_RO, TPID_PO,
-    PRIMARY_REGION_REMAP, NORMAL_REGION_REMAP
-  } MMU_regNAME;
+  static const int PROCESSOR_FEATURE_0          = 0x0010;
+  static const int PROCESSOR_FEATURE_1          = 0x0011;
+  static const int DEBUG_FEATURE_0              = 0x0012;
+  static const int AUXILIARY_FEATURE_0          = 0x0013;
+  static const int MEMORY_MODEL_FEATURE_0       = 0x0014;
+  static const int MEMORY_MODEL_FEATURE_1       = 0x0015;
+  static const int MEMORY_MODEL_FEATURE_2       = 0x0016;
+  static const int MEMORY_MODEL_FEATURE_3       = 0x0017;
 
-  typedef struct
+  static const int INSTRUCTION_SET_ATTRIBUTE_0  = 0x0020;
+  static const int INSTRUCTION_SET_ATTRIBUTE_1  = 0x0021;
+  static const int INSTRUCTION_SET_ATTRIBUTE_2  = 0x0022;
+  static const int INSTRUCTION_SET_ATTRIBUTE_3  = 0x0023;
+  static const int INSTRUCTION_SET_ATTRIBUTE_4  = 0x0024;
+  static const int INSTRUCTION_SET_ATTRIBUTE_5  = 0x0025;
+  static const int INSTRUCTION_SET_ATTRIBUTE_6  = 0x0026;
+  static const int INSTRUCTION_SET_ATTRIBUTE_7  = 0x0027;
+
+  static const int CACHE_SIZE_IDENTIFICATION    = 0x0100;
+  static const int CACHE_LEVEL_ID               = 0x0101;
+  static const int SILICON_ID                   = 0x0107;
+  static const int CACHE_SIZE_SELECTION         = 0x0200;
+  static const int CONTROL                      = 0x1000;
+  static const int AUXILIARY_CONTROL            = 0x1001;
+  static const int COPROCESSOR_ACCESS_CONTROL   = 0x1002;
+  static const int SECURE_CONFIGURATION         = 0x1010;
+  static const int SECURE_DEBUG_ENABLED         = 0x1011;
+  static const int NONSECURE_ACCESS_CONTROL     = 0x1012;
+
+  static const int TRANSLATION_TABLE_BASE_0     = 0x2000;
+  static const int TRANSLATION_TABLE_BASE_1     = 0x2001;
+  static const int TRANSLATION_TABLE_BASE_CONTROL = 0x2002;
+
+  static const int DOMAIN_ACCESS_CONTROL        = 0x3000;
+  static const int DATA_FAULT_STATUS            = 0x5000;
+  static const int INSTRUUCTION_FAULT_STATUS    = 0x5001;
+  static const int DATA_AUXILIARY_FAULT_STATUS  = 0x5010;
+  static const int INSTRUCTION_AUXILIARY_FAULT_STATUS = 0x50011;
+
+  static const int DATA_FAULT_ADDRESS           = 0x6000;
+  static const int INSTRUCTION_FAULT_ADDRESS    = 0x6002;
+
+  static const int NOP_WFI                      = 0x7004;
+  static const int PHYSICAL_ADDRESS             = 0x7040;
+  static const int INVALIDATE_ALL_INSTR_CACHE_TO_UNIFICATION_POINT = 0x7050;
+  static const int INVALIDATE_INSTR_CACHE_LINE_UNIFICATION_POINT = 0x7051;
+  static const int FLUSH_PREFETCH_BUFFER        = 0x7054;
+
+  static const int NOP_INVALIDATE_ENTIRE_BRANCH_PREDICTOR = 0x7056;
+  static const int NOP_INVALIDATE_BRANCH_PREDICTOR_LINE_MVA = 0x7057;
+
+  static const int INVALIDATE_DATA_CACHE_LINE_TO_MVA_POINT = 0x7061;
+  static const int INVALIDATE_DATA_CACHE_LINE_BY_SET_AND_WAY = 0x7062;
+
+  static const int VA_TO_PA_TRANSLATION_IN_CURRENT_STATE = 0x7080;
+  static const int VA_TO_PA_TRANSLATION_IN_OTHER_STATE = 0x7084;
+  static const int CLEAN_DATA_CACHE_LINE_TO_MVA_POINT = 0x70a1;
+
+  static const int CLEAN_DATA_CACHE_LINE_BY_SET_AND_WAY = 0x70a2;
+
+  static const int DATA_SYNCHRONIZATION_BARRIER = 0x70a4;
+  static const int DATA_MEMORY_BARRIER = 0x70a5;
+
+  static const int CLEAN_DATA_CACHE_TO_UNIFICATION_POINT = 0x70b1;
+  static const int CLEAN_AND_INVALIDATE_DATA_CACHE_TO_MVA_POINT = 0x70e1;
+  static const int CLEAN_AND_INVALIDATE_DATA_CACHE_LINE_BY_SET_AND_WAY = 0x70e2;
+
+  static const int INVALIDATE_INSTRUCTION_TLB_UNLOCKED_ENTRIES = 0x8050;
+  static const int INVALIDATE_INSTRUCTION_TLB_ENTRY_BY_MVA = 0x8051;
+  static const int INVALIDATE_INSTRUCTION_TLB_ENTRY_ON_ASID_MATCH = 0x8052;
+
+  static const int INVALIDATE_DATA_TLB_UNLOCKED_ENTRIES = 0x8060;
+  static const int INVALIDATE_DATA_TLB_ENTRY_BY_MVA = 0x8061;
+  static const int INVALIDATE_DATA_TLB_ENTRY_ON_ASID_MATCH = 0x8062;
+
+  static const int INVALIDATE_UNIFIED_TLB_UNLOCKED_ENTRIES = 0x8070;
+  static const int INVALIDATE_UNIFIED_TLB_ENTRY_BY_MVA = 0x8071;
+  static const int INVALIDATE_UNIFIED_TLB_ENTRY_ON_ASID_MATCH = 0x8072;
+
+  static const int PERFORMANCE_MONITOR_CONTROL = 0x90c0;
+  static const int COUNT_ENABLE_SET = 0x90c1;
+  static const int COUNT_ENABLE_CLEAR = 0x90c2;
+  static const int OVERFLOW_FLAG_STATUS = 0x90c3;
+  static const int SOFTWARE_INCREMENT = 0x90c4;
+  static const int PERFORMANCE_COUNTER_SELECTION = 0x90c5;
+  static const int CYCLE_COUNT = 0x90d0;
+  static const int EVENT_SELECTION = 0x90d1;
+  static const int PERFORMANCE_MONITOR_COUNT = 0x90d2;
+  static const int USER_ENABLE = 0x90e0;
+  static const int INTERRUPT_ENABLE_SET = 0x90e1;
+  static const int INTERRUPT_ENABLE_CLEAR = 0x90e2;
+
+  static const int L2_CACHE_LOCKDOWN = 0x9100;
+  static const int L2_CACHE_AUXILIARY_CONTROL = 0x9102;
+
+  static const int DATA_TLB_LOCKDOWN_REGISTER = 0xa000;
+  static const int INSTRUCTION_TLB_LOCKDOWN_REGISTER = 0xa001;
+  static const int DATA_TLB_PRELOAD = 0xa010;
+  static const int INSTRUCTION_TLB_PRELOAD = 0xa011;
+  static const int PRIMARY_REGION_REMAP_REGISTER = 0xa020;
+  static const int NORMAL_MEMORY_REMAP_REGISTER = 0xa021;
+
+  static const int SECURE_OR_NONSECURE_VECTOR_BASE_ADDRESS = 0xc000;
+  static const int MONITOR_VECTOR_BASE_ADDRESS = 0xc001;
+  static const int INTERRUPT_STATUS = 0xc010;
+
+  static const int FCSE_PID = 0xd000;
+  static const int CONTEXT_ID = 0xd001;
+
+  // Struct that stores a coprocessor 15 register.
+
+  struct cp15_register
   {
+    // Value stored in the register.
+
     uint32_t value;
+
+    // Permissions for the register.
+
     unsigned char permissions[2];
-  } cp15_reg;
+
+    // Callback called during a write to this register. It is executed
+    // before writing value.
+
+    void (*write_callback) (struct cp15_register *reg, uint32_t *datum);
+
+    // Callback called during a read to the register. It is executed before
+    // reading value.
+
+    void (*read_callback) (struct cp15_register *reg);
+  };
 
   friend class MMU;
 
 private:
-  cp15_reg SCC_RB[19];
-  cp15_reg MMU_RB[18];
+  struct cp15_register registers[0xFFFF];
 
   void reset ();
 
-  void TLB_operations (const unsigned opc1, const unsigned opc2,
-		       const unsigned crn, const unsigned crm,
-		       const unsigned rt_value);
-
-  cp15_reg *getRegister (const unsigned opc1, const unsigned opc2,
-			 const unsigned crn, const unsigned crm);
+  struct cp15_register *getRegister (const unsigned opc1,
+                                     const unsigned opc2,
+                                     const unsigned crn,
+                                     const unsigned crm);
 public:
-  void MCR (arm_arch_ref * core,
-	    const arm_impl::PrivilegeLevel pl, const unsigned opc1,
-	    const unsigned opc2, const unsigned crn,
-	    const unsigned crm, const unsigned rt_value);
+  void MCR (arm_arch_ref *core, const arm_impl::PrivilegeLevel pl,
+            const unsigned opc1, const unsigned opc2,
+            const unsigned crn,  const unsigned crm,
+            unsigned rt_value);
 
-  uint32_t MRC (arm_arch_ref * core, const arm_impl::PrivilegeLevel pl,
+  uint32_t MRC (arm_arch_ref *core, const arm_impl::PrivilegeLevel pl,
 		const unsigned opc1, const unsigned opc2,
 		const unsigned crn, const unsigned crm);
 
@@ -82,3 +196,4 @@ public:
 };
 
 #endif // !CP15_H.
+
