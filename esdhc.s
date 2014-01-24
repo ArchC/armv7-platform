58a1567fb0b61476b45907dfafff477f396fd543
Gabriel Krisman Bertazi <gabriel@krisman.be>
2014-01-23 23:55:17 -0200

    Rewrite send command interface and adapt source to use new version.

--- esdhc.s
+++ esdhc.s
@@ -2,6 +2,10 @@
 @                                                                          @
 @         i.MX53 SoC ESDHC based IO library                                @
 @                                                                          @
+@       Based on the U-boot esdhc driver code.                             @
+@                                                                          @
+@      By Gabriel Krisman Bertazi       01/2014                            @
+@                                                                          @
 @ -------------------------------------------------------------------------@
 @ --[ Includes ]-----------------------------------------------------------@
 @
@@ -63,25 +67,34 @@ _no_sd_err:
 _end_of_configure_esdhc:
         ldmfd sp!, {pc}
 
-
-@ --[ esdhc_get_response ]-----------------------------------------------@
+@ --[ esdhc_get_response ]-----------------------------------------@
 @
-@   Return Response for a command
+@   Return pointer to response structure.
+@
+@ Returns
+@       R0 - Return pointer to REPONSE_STRUCT
+        .globl esdhc_get_response
+esdhc_get_response:
+        ldr r0, =ESDHC_RESPONSE_STRUCT
+        mov pc, lr
+
+@ --[ esdhc_fill_response_struct ]-----------------------------------------@
+@
+@   Update response structure.
 @
 @ Arguments
 @	R0 - Response type
 @
 @ Returns
 @       R0 - Return pointer to REPONSE_STRUCT
-.globl esdhc_get_response
-esdhc_get_response:
+esdhc_fill_response:
         stmfd sp!, {r4-r9, lr}
         ldr r4, =ESDHC_BASE
         ldr r1, =ESDHC_RESPONSE_STRUCT
         mov r9, r1
 
         tst r0, #MMC_RSP_136
-        beq _get_response_simple
+        beq _fill_response_struct_simple
 
         ldr r8, [r4, #ESDHC_CMDRSP3]
         ldr r7, [r4, #ESDHC_CMDRSP2]
@@ -107,14 +120,14 @@ esdhc_get_response:
         mov r2, r5, lsl #0x8
         str r2, [r1], #4
 
-        b _end_of_get_response
+        b _end_of_fill_response_struct
 
-_get_response_simple:
+_fill_response_struct_simple:
         @cmd->response[0]
         ldr r2, [r4, #ESDHC_CMDRSP0]
         str r2, [r1]
 
-_end_of_get_response:
+_end_of_fill_response_struct:
         mov r0, r9
         ldmfd sp!, {r4-r9, pc}
 
@@ -131,6 +144,162 @@ card_connected:
         ldr r0, [r0, #ESDHC_PRSSTAT]
         and r0, r0, #PRSSTAT_CINS
         mov pc, lr
+
+@ --[ esdhc_set_block_count ]-----------------------------------------------@
+@
+@ Set block count on BLKATTR register.
+@
+@ Arguments
+@        R0 - Block count
+esdhc_set_block_count:
+        ldr r1, =ESDHC_BASE
+        ldr r2, [r1, #ESDHC_BLKATTR]
+        ldr r3, =0xfff
+        and r2, r2, r3
+        orr r2, r2, r0, lsl #16
+        str r2, [r1, #ESDHC_BLKATTR]
+        mov pc, lr
+@ --[ esdhc_set_block_count ]-----------------------------------------------@
+@
+@ Get block count on BLKATTR register.
+@
+@ Returns:
+@        R0 - Block count
+esdhc_get_block_count:
+        ldr r1, =ESDHC_BASE
+        ldr r0, [r1, #ESDHC_BLKATTR]
+        lsr r0, #16
+        mov pc, lr
+
+@ --[ esdhc_send_command ]-----------------------------------------------@
+@
+@   Send command to bus
+@
+@ Argument
+@       R0 - CMD index
+@       R1 - Argument
+@       R2 - RSPTYPE
+@       r3 - Pointer to data region. Might be NULL if
+@            command does not require data.
+@ Returns
+@       R0 - Pointer to  cmdresp data.
+
+        .globl esdhc_send_command
+esdhc_send_command:
+        stmfd sp!, {r4-r6, lr}
+        ldr r4, =ESDHC_BASE
+
+        @ Clear ESDHC_IRQSTAT
+        mov r5, #0
+        neg r5, r5
+        str r5, [r4, #ESDHC_IRQSTAT]
+
+        @Activate SDHCI_IRQ_EN_BITS
+        ldr r5, [r4, #ESDHC_IRQSTATEN]
+        ldr r6, =SDHCI_IRQ_EN_BITS
+        orr r5, r5, r6
+        str r5, [r4, #ESDHC_IRQSTATEN]
+
+        @ Wait for bus to be idle
+        @ (Wait for PRSSTAT{CICHB, CIDHB, DLA} to be down)
+_esdhc_send_cmd_while_not_idle:
+        mov r6, #PRSSTAT_CICHB
+        ldr r5, [r4, #ESDHC_PRSSTAT]
+        orr r6, r6, #PRSSTAT_CIDHB
+        orr r6, r6, #PRSSTAT_DLA
+        tst r5, r6
+        bne _esdhc_send_cmd_while_not_idle
+
+        @Prepare string XFERTYP
+        mov r0, r0, lsl  #24            @Command Index
+        orr r0, r0, r0                  @cmd type  <<FIX
+        orr r0, r0, r2, LSL #16         @ Response Type
+
+        @ Send argument
+        str r1, [r4, #ESDHC_CMDARG]
+
+        @ Send command
+        str r0, [r4, #ESDHC_XFERTYP]
+
+        @Mask All IRQs
+        mov r0, #0
+        str r0, [r4, #ESDHC_IRQSIGEN]
+
+        @Wait for command to complete.
+_esdhc_send_cmd_while_no_complete:
+        ldr r0, [r4, #ESDHC_IRQSTAT]
+        ldr r1, =CMD_FINISHED
+        tst r0, r1
+        beq _esdhc_send_cmd_while_no_complete
+
+        @ Check for errors
+        tst r0, #CMD_ERR
+        movne r0, #ERROR_SD_CMD_ERROR
+        bne _esdhc_send_cmd_end
+
+        tst r0, #IRQSTAT_CTOE
+        movne r0, #ERROR_SD_CMD_TIMEOUT
+        bne _esdhc_send_cmd_end
+
+        @ Fill response structure
+        mov r0, r2                @Response type
+        bl esdhc_fill_response
+
+        @ Still need to handle data.
+        cmp r3, #0
+        beq _esdhc_send_cmd_end_of_data_fetch
+_esdhc_send_cmd_data_fetch:
+        @Activate SDHCI_IRQ_EN_BITS
+        ldr r5, [r4, #ESDHC_IRQSTATEN]
+        ldr r6, =SDHCI_IRQ_EN_BITS
+        orr r5, r5, r6
+        str r5, [r4, #ESDHC_IRQSTATEN]
+
+        mov r5, #0
+        ldr r6, [r4, #ESDHC_BLKATTR]
+        lsr r6, #16
+_esdhc_send_cmd_FOR_blk_count:
+        cmp r5, r6
+        bge _esdhc_send_cmd_end_of_FOR_blk_count
+
+____esdhc_send_cmd_wait_brr:
+        ldr r0, [r4, #ESDHC_IRQSTAT]
+        tst r0, #IRQSTAT_BRR
+        beq ____esdhc_send_cmd_wait_brr
+
+        mov r7, #0
+        ldr r8, [r4, #ESDHC_BLKATTR]
+        ldr r0 , =0xFFF
+        and r8, r8, r0
+        lsr r8, #2
+____esdhc_send_cmd_for_blksize:
+        cmp r7, r8
+        bge ____esdhc_send_cmd_end_of_for_blksize
+
+        ldr r2, [r4, #ESDHC_DATPORT]
+        str r2, [r3], #4
+
+        add r7, r7, #1
+        b ____esdhc_send_cmd_for_blksize
+____esdhc_send_cmd_end_of_for_blksize:
+
+        @ Clear IRQSTAT
+        ldr r0, [r4, #ESDHC_IRQSTAT]
+        orr r0, r0, #IRQSTAT_BRR
+        str r0, [r4, #ESDHC_IRQSTAT]
+
+        add r5, r5, #1
+        b _esdhc_send_cmd_FOR_blk_count
+_esdhc_send_cmd_end_of_FOR_blk_count:
+_esdhc_send_cmd_end_of_data_fetch:
+       @ Clear ESDHC_IRQSTAT and terminates.
+        mov r5, #0
+        neg r5, r5
+        str r5, [r4, #ESDHC_IRQSTAT]
+
+        mov r0, #0
+_esdhc_send_cmd_end:
+        ldmfd sp!, {r4-r6, pc}
 
 _STRING_ERROR_NO_SD_CARD_:
         .asciz "ERROR: MMC: No SD card found\n"
--- sd.s
+++ sd.s
@@ -2,7 +2,9 @@
 @                                                                          @
 @         i.MX53 SoC SD card Controller driver                             @
 @                                                                          @
-@      By Gabriel Krisman Bertazi    19/12/2012                            @
+@       Based on the U-boot esdhc driver code.                             @
+@                                                                          @
+@      By Gabriel Krisman Bertazi       01/2014                            @
 @                                                                          @
 @ -------------------------------------------------------------------------@
 @ --[ Includes ]-----------------------------------------------------------@
@@ -62,77 +64,6 @@ init_sd:
 _end_of_init_sd:
         ldmfd sp!, {pc}
 
-@ --------------------------------------------------------------------------------
-@ Expects:
-@  'r0' Destination address
-@  'r1' SD card start address
-@  'r2' Number of blocks to be read.globl sd_readSingleblock
-@  'r3' Watermark Lvl
-@
-@ WARNING: Blocksize must be set prior calling this function
-.globl sd_readblock
-sd_readblock:
-        stmfd sp!, {r0,r4-r9, lr}
-
-        @set cmdarg
-        bl wait_cihb
-        ldr r4, =ESDHC_BASE
-        str r1, [r4, #ESDHC_CMDARG]
-
-
-        movw r8, #0x1fff
-        ldr r7, [r4, #ESDHC_BLKATTR]
-        and r7, r7, r8         @R5 agora tem o block size
-        mul r6, r2, r7        @ r6 tem o numero de bytes
-
-        @set BLKATTR  <- BLKSIZE must be previously set
-        mov r2, r2, LSL #16   @Calculate BLKATTR
-        orr r7, r7, r2
-        str r7, [r4, #ESDHC_BLKATTR]
-        mov r5, r6
-
-        @Prepare XFERTYP
-        mov r1, #18
-        mov r1, r1, LSL #24
-        ldr r2, =0x210036            @mask{DPSEL|RSTYP[0]|MSBSEL|DTDSEL|AC12EN|BCEN}
-        orr r1, r1, r2
-
-        bl wait_cihb
-        str r1, [r4, #ESDHC_XFERTYP]
-
-__readblk_wait_data_ready:
-        ldr r1, [r4, #ESDHC_PRSSTAT]
-        tst r1, #0x800
-        beq __readblk_wait_data_ready
-
-        mov r6, r3   @WML lvl
-___read_until_wml:
-        ldr r1, [r4, #ESDHC_DATPORT]
-        str r1, [r0], #4
-
-        subs r5, r5, #4
-        nop                @no pipeline issues
-        beq __readblk_end
-
-        subs r6, r6, #0x4
-        nop                @no pipeline issues
-        bne ___read_until_wml
-        b __readblk_wait_data_ready
-
-        mov r0, #0
-__readblk_end:
-         ldmfd sp!, {r0, r4-r9, pc}
-
- @ --------------------------------------------------------------------------------
- @ Expects:
- @  'r0' buffer pointer to bytes to be written
- @  'r1' SD card start address to be written to
- @  'r1' number of chars to write
-
- .globl sd_writeblock
- sd_writeblock:
-         mov pc, lr
-
  @ --------------------------------------------------------------------------------
  @ Expects: 'r0' Block length
  @ returns: 'r0' Result Status 0 if success
@@ -143,12 +74,12 @@ __readblk_end:
  .globl sd_set_blockLen
  sd_set_blockLen:
          stmfd sp!, {lr}
-         mov r1, r0    @ARG
-         mov r2, #00   @CMDTYP
-         mov r3, #01   @RSPTYP
+         mov r1, r0     @ARG
+         mov r2, #0x1   @CMDTYP
+         mov r3, #0x0   @RSPTYP
  __setblklen_wait_cihb:
          mov r0, #16   @CMD_16 => SET_BLKLEN
-         bl sd_send_command
+         bl esdhc_send_command
          cmp r0, #0
          bne __setblklen_wait_cihb
 
@@ -157,96 +88,7 @@ __readblk_end:
          mov r0, #0      @return code
          ldmfd sp!, {pc}
 
- @ --------------------------------------------------------------------------------
- @ Expects: 'r0' CMD INDEX
- @          'r1' ARGUMENT
- @          'r2' CMDTYPE
- @          'r3' RSPTYPE
- @
- @ returns: 'r0' Result Status 0 if success
- @
- @ Send a command to ESDHC by writing to XFERTYP register
- @
- sd_send_command:
-         stmfd sp!, {r4-r9, lr}
-         ldr r4, =ESDHC_BASE
 
-         @Verify ESDHC ready for input by checking CIHB on PRSSTAT
-         ldr r5, [r4, #ESDHC_PRSSTAT]
-         tst r5, #0b1                    @ Verify bit 0
-         movne r0, #1                    @ Failure return code
-         bne _send_cmd_end               @ If cant write end function
-
-         @Send Argument
-         str r1, [r4, #ESDHC_CMDARG]     @Store argument
-
-         @Prepare string XFERTYP
-         mov r0, r0, LSL  #24
-         orr r0, r0, r2, LSL #22
-         orr r0, r0, r3, LSL #16
-         str r0, [r4, #ESDHC_XFERTYP]
-
-         mov r0, #0 @sucess return code
-_send_cmd_end:
-         ldmfd sp!, {r4-r9,pc}
-
- @ --------------------------------------------------------------------------------
- @ Expects: 'r0' WR_BRST_LEN
- @          'r1' WR_WML
- @          'r2' RD_BRST_LEN
- @          'r3' RD_WML
- @
- @ Set ESDHC watermarkLevel
- @
- sd_set_watermark_level:
-         and r0, r0, #0b11111
-         mov r0, r0, LSL #24
-
-         and r1, r1, #0xFF
-         orr r0, r0, r1, LSL #16
-
-         and r2, r2, #0b11111
-         orr r0, r0, r2, LSL #8
-
-         and r3, r3, #0xFF
-         orr r0, r0,r3
-
-         ldr r1, =ESDHC_BASE
-         str r0, [r1, #ESDHC_WML]
-         mov pc, lr
-
- @ --------------------------------------------------------------------------------
- @ Expects: 'r0' Block Count
- @          'r1' Block Size
- @
- @ Set the block length of ESDHC writing to BlockAttr reg
- @
-        .globl  esdhc_setBlockAttr
- esdhc_setBlockAttr:
-
-         mov r0, r0, LSL #16
-
-         movw r2, #0x1fff
-         and r1, r1, r2
-
-         orr r0, r0, r1
-         ldr r2, =ESDHC_BASE
-         str r0, [r2, #ESDHC_BLKATTR]
-         mov pc, lr
-
-@ --------------------------------------------------------------------------------
-@ wait_cihb
-@
-@
-@
-wait_cihb:
-       stmfd sp!, {r4,lr}
-_cihb_not_ready:
-       ldr r4, =ESDHC_BASE
-       ldr r4, [r4, #ESDHC_PRSSTAT]
-       tst r4, #0b1
-       bne _cihb_not_ready
-       ldmfd sp!, {r4,pc}
 
 @ --------------------------------------------------------------------------------
 @ sd_go_idle
@@ -259,10 +101,10 @@ _cihb_not_ready:
 sd_go_idle:
         stmfd sp!, {lr}
         mov r0, #MMC_CMD_GO_IDLE_STATE
-        mov r1, #0
+        mov r1, #0x0
         mov r2, #MMC_RSP_NONE
-        mov r3, #MMC_RSP_NONE
-        bl sd_send_command
+        mov r3, #0x0
+        bl esdhc_send_command
         ldmfd sp!, {pc}
 
 @ --------------------------------------------------------------------------------
@@ -274,9 +116,9 @@ sd_set_relative_address:
         stmfd sp!, {lr}
         mov r0, #SD_CMD_SEND_RELATIVE_ADDR
         mov r1, #0x0
-        mov r2, #0x0
-        mov r3, #MMC_RSP_R6
-        bl sd_send_command
+        mov r2, #MMC_RSP_R6
+        mov r3, #0x0
+        bl esdhc_send_command
 
         @ Verify return.
         mov r0, #0
@@ -304,9 +146,9 @@ sd_select_card:
         mov r1, r0  @ rca argument
         mov r0, #MMC_CMD_SELECT_CARD
         mov r1, r1, lsl #16
-        mov r2, #0x0
-        mov r3, #MMC_RSP_R1
-        bl sd_send_command
+        mov r2, #MMC_RSP_R1
+        mov r3, #0x0
+        bl esdhc_send_command
 
         mov r0, #0
         ldmfd sp!, {pc}
@@ -324,9 +166,9 @@ sd_send_if_cond:
         mov r0, #SD_CMD_SEND_IF_COND
         mov r1, #0x100
         orr r1, r1, #0xaa         @ Verification bits
-        mov r2, #0x0
-        mov r3, #MMC_RSP_R7
-        bl sd_send_command
+        mov r2, #MMC_RSP_R7
+        mov r3, #0x0
+        bl esdhc_send_command
 
         @ Verify return.
         mov r0, #0x0
@@ -351,16 +193,16 @@ _do_while_op_cond_wait:
         @ Issue APP_CMD
         mov r0, #MMC_CMD_APP_CMD
         mov r1, #0x0
-        mov r2, #0x0
-        mov r3, #MMC_RSP_R1
-        bl sd_send_command
+        mov r2, #MMC_RSP_R1
+        mov r3, #0x0
+        bl esdhc_send_command
 
         @ Issue APP_SEND_OP_COND
         mov r0, #SD_CMD_APP_SEND_OP_COND
         mov r1, #0x0
-        mov r2, #0x0
-        mov r3, #MMC_RSP_R3
-        bl sd_send_command
+        mov r2, #MMC_RSP_R3
+        mov r3, #0x0
+        bl esdhc_send_command
 
         bl sd_get_response
         tst r0, #OCR_BUSY
@@ -379,9 +221,9 @@ sd_get_cid:
         @ Identification
         mov r0, #MMC_CMD_ALL_SEND_CID
         mov r1, #0x0
-        mov r2, #0x0
-        mov r3, #MMC_RSP_R2
-        bl sd_send_command
+        mov r2, #MMC_RSP_R2
+        mov r3, #0x0
+        bl esdhc_send_command
         cmp r0, #0
         movne r0, #ERROR_SD_INIT_FAIL
         bne _end_of_init_sd
@@ -393,6 +235,7 @@ sd_get_cid:
 
         mov r0, #0
         ldmfd sp!, {r4, pc}
+        
 @ --------------------------------------------------------------------------------
 @ Get Response
 @
@@ -412,24 +255,11 @@ sd_get_response:
 @ sd_load_block
 @
 @ Argument
-@   r0 - destination address
+@   r0 - destinati on address
 @   r1 - Block
 @   r2 - Number of blocks
         .globl sd_load_block
 sd_load_block:
         stmfd sp!, {lr}
-
-        stmfd sp!, {r0-r2}
-	@SD blocklen
-        mov r0, #0x4 @ Set block to 512
-        bl  sd_set_blockLen
-        mov r0, #0x2
-        mov r1, #0x4 @ Must be same as blockLen
-        bl esdhc_setBlockAttr
-
-        @ldr self value
-        ldmfd sp!, {r0-r2}
-        mov r3, #1  @wml
-        bl sd_readblock
         ldmfd sp!, {pc}
 
